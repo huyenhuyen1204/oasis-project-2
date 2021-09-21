dbe898b48a4f14c239bb602db691b85e8a486e25
Huyen1204 <nguyenhuyen98pm@gmail.com>
2021-09-21 09:57:52 +0700

    first commit hdkashda

---
+++ README.md
@@ -0,0 +1,9 @@
+"# oasis-project-2" 
+#Git Demo
+## environment?
+- java 1.8
+- Intellij
+## How to run
+
+
+
---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    /**
+     * this is main function.
+     * @param args argument.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
